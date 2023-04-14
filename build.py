8c05beb18eaeb6b292ce95625c6c89d2c9d0158d
Animal <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-04-14 14:14:31 -0400

    Update build.py

--- build.py
+++ build.py
@@ -1,3 +1,10 @@
 import os
+import shutil
 
-os.system("pyinstaller --collect-all chatterbot --distpath ./bin ./src/main.py")
+#os.system("pyinstaller --collect-all chatterbot --distpath ./bin ./src/main.py")
+
+#shutil.copy("BuildData/customtkinter", "bin/main")
+os.mkdir("bin/main/customtkinter")
+os.system("xcopy BuildData\customtkinter bin\main /e /i")
+#print(os.path.dirname("BuildData/customtkinter"))
+#os.system("xcopy BuildData\customtkinter bin\main /e /i")
