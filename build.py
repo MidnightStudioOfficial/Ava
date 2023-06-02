525fd4dd11ddc3f9a7d7cb6e0da794d45b99ecfb
Animal <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-06-02 14:33:46 -0400

    Build Fix

--- build.py
+++ build.py
@@ -7,12 +7,14 @@ os.system("pyinstaller --collect-all chatterbot --distpath ./bin ./src/main.py")
 
 # Create bin dirs
 os.mkdir("bin/main/customtkinter")
+os.mkdir("bin/main/en_core_web_sm")
 #os.mkdir("bin/main/assets")
 #os.mkdir("bin/main/training")
 os.mkdir("bin/main/Data")
 
 # Copying all the files
 os.system("xcopy BuildData\customtkinter bin\main\customtkinter /e /i")
+os.system("xcopy BuildData\en_core_web_sm bin\main\en_core_web_sm /e /i")
 #os.system("xcopy BuildData\assets bin\main\assets /e /i")
 #os.system("xcopy BuildData\training bin\main\training /e /i")
 os.system("xcopy BuildData\Data bin\main\Data /e /i")
