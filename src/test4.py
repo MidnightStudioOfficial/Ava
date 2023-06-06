7a3c4c219796d06c084ab31a58b1243350a7628f
Animal <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-06-06 15:08:16 -0400

    Update

--- src/test4.py
+++ src/test4.py
@@ -34,3 +34,18 @@ for t in threads:
     t.join()
 
 print("DONE")
+
+class Importer:
+    def __init__(self, modules) -> None:
+        self.modules = modules
+        self.threads = []
+        
+    def import_module(self, package_name, pack):
+        import importlib
+        print("Importing "+str(package_name))
+        globals()[package_name] = importlib.import_module(package_name, package=pack)
+    def import_all(self):
+        for n in self.modules:
+          t = threading.Thread(target=self.import_module, args=(self.modules[n]["package_name"], self.modules[n]["pack"]))
+          self.threads.append(t)
+          t.start()
